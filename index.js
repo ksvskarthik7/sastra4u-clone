20c2b24568654ffa8d19de07cadca3a9635bda1d
SatyaKarthik <satyakarthik2020@gmail.com>
2023-07-07 22:48:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let random_no = Math.floor((Math.random() * 10) + 1);
+console.log(random_no)
+let twiceQuotes = [
+    "You only live once, but if you do it right, once is enough.",
+    "When you smile, the whole world stops and stares for a while.",
+    "Fancy you, ooh.",
+    "Wake up, the sleeping beauty within you.",
+    "We don't stop until the sun rises.",
+    "Turn it up, turn it up, let's make it louder.",
+    "Like a sweet candy, I'll fill you up without you noticing.",
+    "Knock knock knock on my door, oh oh oh.",
+    "I'm like TT, just like TT.",
+    "Feel special, no matter how people look at you.",
+    "Life is tough, my darling, but so are you.",
+  ];
+
+document.getElementById("part1").innerText=twiceQuotes[random_no]
+
+
+  
